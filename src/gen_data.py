2c404acc04b179cfa7ff6963844cb3d7eb314207
chiumax <dumblole@gmail.com>
2022-05-13 07:05:38 -0400

    basic

---
+++ src/gen_data.py
@@ -0,0 +1,23 @@
+"""
+Goals
+- figure out how to use sapai
+- generate data
+    - randomized teams against other randomized teams
+        - team 1
+            - list of the 5 animals
+        - team 2
+            - list of the 5 animals
+        - which team won
+    - randomized pet against other randomized pet
+
+possible analysis
+- if we see a pet that has bad performance but 
+  good performance in team environment - good support animal
+- if we see a pet that has good performance (1v1) but
+  bad performance in team environment - bad support animal
+
+- pet positioning make s a difference
+
+- common pets in teams that win
+- what sort of ability is best ?
+"""
